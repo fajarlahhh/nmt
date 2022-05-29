46f322f102c79f0160f87b229fc5352d51b19851
Andi Fajar Nugraha <33441205+fajarlahhh@users.noreply.github.com>
2022-05-29 21:56:42 +0800

    baru

--- app/Http/Livewire/Member/Downline/Form.php
+++ app/Http/Livewire/Member/Downline/Form.php
@@ -29,7 +29,6 @@ class Form extends Component
       'information' => 'required',
     ]);
 
-    dd(Deposit::where('id', $this->deposit->first()->id)->where('requisite', 'Enrollment')->whereNull('processed_at')->whereNull('information')->get());
     DB::transaction(function () {
       Deposit::where('id', $this->deposit->first()->id)->where('requisite', 'Enrollment')->whereNull('processed_at')->whereNull('information')->update([
         'information' => $this->information,
