9e525d9da5fa03461ed4076c657d03cb10dbf6c9
Andi Fajar Nugraha <33441205+fajarlahhh@users.noreply.github.com>
2022-06-01 21:54:17 +0800

    pin

--- resources/views/includes/footer.blade.php
+++ resources/views/includes/footer.blade.php
@@ -1,4 +1,3 @@
-<div class="intro-x mt-5 text-center xl:text-left">
-  <a href="mailto:support@solutionstake.com" class="text-dark pr-5"><u>E-Mail</u></a>
-  <a href="https://t.me/andrewsolutionstake" target="_blank" class="text-dark pr-5"><u>Telegram</u></a>
+<div id="footer" class="footer text-center">
+  © 2022 Nice Metavest
 </div>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -85,6 +85,8 @@
     </div>
     <!-- end #content -->
 
+    @include('includes.footer')
+
     <!-- begin scroll to top btn -->
     <a href="javascript:;" class="btn btn-icon btn-circle btn-success btn-scroll-to-top fade" data-click="scroll-top"><i
         class="fa fa-angle-up"></i></a>
@@ -97,8 +99,25 @@
   <!-- ================== BEGIN BASE JS ================== -->
   <script src="/assets/js/app.min.js"></script>
   <script src="/assets/js/theme/google.min.js"></script>
+  <script src="/assets/plugins/jquery-migrate/dist/jquery-migrate.min.js"></script>
+  <script src="/assets/plugins/moment/min/moment.min.js"></script>
+  <script src="/assets/plugins/bootstrap-datepicker/dist/js/bootstrap-datepicker.js"></script>
+  <script src="/assets/plugins/ion-rangeslider/js/ion.rangeSlider.min.js"></script>
+  <script src="/assets/plugins/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.min.js"></script>
+  <script src="/assets/plugins/jquery.maskedinput/src/jquery.maskedinput.js"></script>
+  <script src="/assets/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js"></script>
+  <script src="/assets/plugins/pwstrength-bootstrap/dist/pwstrength-bootstrap.min.js"></script>
+  <script src="/assets/plugins/@danielfarrell/bootstrap-combobox/js/bootstrap-combobox.js"></script>
+  <script src="/assets/plugins/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
+  <script src="/assets/plugins/tag-it/js/tag-it.min.js"></script>
+  <script src="/assets/plugins/bootstrap-daterangepicker/daterangepicker.js"></script>
+  <script src="/assets/plugins/select2/dist/js/select2.min.js"></script>
+  <script src="/assets/plugins/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
   <script src="/assets/plugins/bootstrap-show-password/dist/bootstrap-show-password.js"></script>
-  <script src="/assets/plugins/parsleyjs/dist/parsley.js"></script>
+  <script src="/assets/plugins/bootstrap-colorpalette/js/bootstrap-colorpalette.js"></script>
+  <script src="/assets/plugins/jquery-simplecolorpicker/jquery.simplecolorpicker.js"></script>
+  <script src="/assets/plugins/clipboard/dist/clipboard.min.js"></script>
+  <script src="/assets/js/demo/form-plugins.demo.js"></script>
   <!-- ================== END BASE JS ================== -->
 
   @stack('scripts')
